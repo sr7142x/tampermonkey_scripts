24d67c3d745cc49872de9c8f0f9831332dcfaa78
drew <github@andbrant.com>
2026-03-12 09:21:26 -0500

    fix fastmail script

--- my_fastmail_user.user.js
+++ my_fastmail_user.user.js
@@ -3,9 +3,10 @@
 // @namespace   andbrant
 // @match       https://www.fastmail.com/login*
 // @match       https://www.fastmail.com/*
+// @match       https://app.fastmail.com/mail/Inbox/*
 // @grant       GM_getValue
 // @grant       GM_setValue
-// @version     1.0
+// @version     1.1
 // @author      -
 // @dont-require     https://github.com/yeltnar/tampermonkey_scripts/raw/master/timeoutPromise.notauser.js
 // @dont-require     https://github.com/yeltnar/tampermonkey_scripts/raw/master/textEleSearch.notauser.js
@@ -13,7 +14,6 @@
 // ==/UserScript==
 
 (()=>{
-  
   const stored_user = GM_getValue("u");
   const qp_user = new URLSearchParams(window.location.search).get("u");
   
@@ -22,9 +22,12 @@
   if( qp_user !== undefined && qp_user !== null ){
     console.log('Updating fastmail user to ${qp_user}');
     GM_setValue("u",qp_user);
-  }else if( stored_user !== undefined && /login/.test(window.location.href) ){ // if have a stored user, and its not in the URL 
-    console.log('moving to stored user ${stored_user}');
-    window.location.href = `https://www.fastmail.com/mail/Inbox?u=${stored_user}`;
   }
-  
+
+  // if have a stored user, and the path needs to change
+  if( stored_user !== undefined && (/login/.test(window.location.href) || window.location.href==="https://www.fastmail.com/" ) ){
+    console.log('moving to stored user ${stored_user}');
+    window.location.href = `https://app.fastmail.com/mail/Inbox?u=${stored_user}`;
+  }
 })();
+
